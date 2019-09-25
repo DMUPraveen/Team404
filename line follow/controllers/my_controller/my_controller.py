f00d373e4074782cfe68b2eecb2aefac7a548648
DMUPraveen <55706055+DMUPraveen@users.noreply.github.com>
2019-09-26 00:19:09 +0530

    The trick is to use a distance sensor in IR mode pointed down
    light sensors cannot detect reflected light but since ir sensors are sensitive to color we can exploit that to get line following behaviour
    but I think it would be better to use a camera

---
+++ line follow/controllers/my_controller/my_controller.py
@@ -0,0 +1,77 @@
+from controller import Robot , DistanceSensor 
+robot=Robot() 
+sensor1=DistanceSensor("sensor1")
+sensor2=DistanceSensor("sensor2")
+sensor3 =DistanceSensor("sensor3")
+wheel=[] 
+wheel_names=["wheel1","wheel2","wheel3","wheel4"]
+TIME_STEP=64
+for i in range(4):
+    wheel.append(robot.getMotor(wheel_names[i]))
+    wheel[i].setPosition(float('inf'))
+    wheel[i].setVelocity(0.0)
+    #print(wheel[i])
+limit=801
+def forward(speed=10):
+  
+    wheel[0].setVelocity(speed)
+    wheel[1].setVelocity(speed)
+    wheel[2].setVelocity(speed)
+    wheel[3].setVelocity(speed)
+
+def rotateL():
+    wheel[0].setVelocity(-1)
+    wheel[1].setVelocity(1)
+    wheel[2].setVelocity(-1)
+    wheel[3].setVelocity(1)
+    
+def rotateR():
+    wheel[0].setVelocity(1)
+    wheel[1].setVelocity(-1)
+    wheel[2].setVelocity(1)
+    wheel[3].setVelocity(-1)
+
+
+
+def stop():
+    for i in range(0,4):
+        wheel[i].setVelocity(0.0)
+#rotate()       
+while robot.step(TIME_STEP)!= -1:
+    sensor1.enable(TIME_STEP)
+    sensor2.enable(TIME_STEP)
+    sensor3.enable(TIME_STEP)
+    x = sensor1.getValue()
+    y = sensor2.getValue()
+    z = sensor3.getValue()
+    #print("{},{}".format(str(x),str(y)))
+    '''if(z>x and z>y):
+        forward()
+    if(x>z and x>y):
+        rotateL()
+    if(y>z and y>x):
+        rotateR()
+    else:
+        forward()'''
+        
+    print("{},{},{}".format(str(x),str(z),str(y)))
+    
+    if z >900:
+        forward()
+    elif(x>z and x>y):
+        rotateR()
+    elif(y>z and y>x):
+        rotateL()
+    else:
+        rotateL()
+        
+
+    
+    #print(k)
+
+        
+        
+
+    #print(sensorL.getValue())
+ 
+    
